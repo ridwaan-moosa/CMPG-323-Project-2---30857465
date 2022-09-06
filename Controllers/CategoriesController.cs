ef17b094096fd096acf9b5e25fcbdcd8a4c3bb60
ridwaan <ridwaan91@yahoo.com>
2022-09-06 17:10:39 +0200

    git commit to project2 repo

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using _30857465_project2.Models;
+using Microsoft.AspNetCore.Authorization;
+using JWTAuthentication.Authentication;
+
+namespace _30857465_project2.Controllers
+{
+    [Authorize(Roles = UserRoles.Admin)]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly project2dbdevContext _context;
+
+        public CategoriesController(project2dbdevContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Categories
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Category>>> GetCategory()
+        {
+            return await _context.Category.ToListAsync();
+        }
+
+        // GET: api/Categories/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Category>> GetCategory(Guid id)
+        {
+            var category = await _context.Category.FindAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return category;
+        }
+
+        // PUT: api/Categories/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCategory(Guid id, Category category)
+        {
+            if (id != category.CategoryId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(category).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Categories
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Category>> PostCategory(Category category)
+        {
+            _context.Category.Add(category);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (CategoryExists(category.CategoryId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetCategory", new { id = category.CategoryId }, category);
+        }
+
+        // DELETE: api/Categories/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Category>> DeleteCategory(Guid id)
+        {
+            var category = await _context.Category.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _context.Category.Remove(category);
+            await _context.SaveChangesAsync();
+
+            return category;
+        }
+
+        private bool CategoryExists(Guid id)
+        {
+            return _context.Category.Any(e => e.CategoryId == id);
+        }
+        //Create a GET method that retrieves all devices within a specific category (based on the category ID that is parsed through)
+        //will only return one item (resolve)
+        [HttpGet("{CategoryId}")]
+        public async Task<ActionResult<Device>> GetDevice([FromBody] Device CategoryId)
+        {
+            var device = await _context.Device.FindAsync(CategoryId);
+
+            if (device == null)
+            {
+                return NotFound();
+            }
+
+            return device;
+        }
+    }
+}
---
+++ Models/project2dbdevContext.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace _30857465_project2.Models
+{
+    public partial class project2dbdevContext : DbContext
+    {
+        public project2dbdevContext()
+        {
+        }
+
+        public project2dbdevContext(DbContextOptions<project2dbdevContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Category> Category { get; set; }
+        public virtual DbSet<Device> Device { get; set; }
+        public virtual DbSet<Zone> Zone { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Password=0165562314Rm;Persist Security Info=True;User ID=serviceadmin;Initial Catalog=project2db-dev;Data Source=ridwaancmpg323sql.database.windows.net");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.Property(e => e.CategoryId)
+                    .HasColumnName("CategoryID")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.CategoryName).IsRequired();
+
+                entity.Property(e => e.DateCreated)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+            });
+
+            modelBuilder.Entity<Device>(entity =>
+            {
+                entity.Property(e => e.DeviceId)
+                    .HasColumnName("DeviceID")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+
+                entity.Property(e => e.DateCreated)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.ZoneId).HasColumnName("ZoneID");
+            });
+
+            modelBuilder.Entity<Zone>(entity =>
+            {
+                entity.Property(e => e.ZoneId)
+                    .HasColumnName("ZoneID")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.DateCreated)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.ZoneName).IsRequired();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
